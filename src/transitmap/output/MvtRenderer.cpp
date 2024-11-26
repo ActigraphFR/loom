6f13cc59fca468427b1426bc7cb4d9841f7da47f
Nicolas LAFOREST <19171430+ThierryDH@users.noreply.github.com>
2024-11-26 14:27:53 +0100

    Update MvtRenderer.cpp

--- src/transitmap/output/MvtRenderer.cpp
+++ src/transitmap/output/MvtRenderer.cpp
@@ -35,21 +35,19 @@ using util::geo::LinePointCmp;
 using util::geo::Polygon;
 using util::geo::PolyLine;
 
-const static double TILE_RES = 1024;
+const static double TILE_RES = 512;
 
 // ground width/height of a single tile on zoom level 0
 const static double WEB_MERC_EXT = 20037508.3427892;
 
 // ground width/height is halved with each zoom level
-// const static int GRID_ZOOM = 14;
-const static int GRID_ZOOM = 4;
+const static int GRID_ZOOM = 14;
 const static int GRID_SIZE = 1 << (GRID_ZOOM);
 
 const static double GRID_W =
     (WEB_MERC_EXT * 2) / static_cast<double>(GRID_SIZE);
 
-//const static int GRID2_ZOOM = 7;
-const static int GRID2_ZOOM = 2;
+const static int GRID2_ZOOM = 7;
 const static int GRID2_SIZE = 1 << (GRID2_ZOOM);
 
 const static double GRID2_W =
